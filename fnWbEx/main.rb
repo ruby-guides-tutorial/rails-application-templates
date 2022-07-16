b6ed07d1723d8259b36191891767ff1a8c49545e
guxiaobai <sikuan.gu@gmail.com>
2022-07-16 20:07:58 +0800

    Rspec

--- fnWbEx/main.rb
+++ fnWbEx/main.rb
@@ -4,8 +4,24 @@ end
 
 run_bundle
 
+run "bundle binstub rspec-core"
+
 generate 'rspec:install'
 uncomment_lines 'spec/rails_helper.rb', /support.*require/
+
+
+# inject_into_class 'config/application.rb', 'Application' do
+#   <<-EOS.strip_heredoc.indent(4)
+#     # Setting for Rspec
+#     config.generators do |g|
+#       g.test_framework :rspec,
+#         view_specs: false,
+#         helper_specs: false,
+#         routing_specs: false,
+#         controller_specs: false
+#     end
+#   EOS
+# end
 
 git add: "."
 git commit: %Q{ -m 'RSpec' }
