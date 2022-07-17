a810ea42dbe5b392415a2e977dbdadd1bb1d4e27
guxiaobai <sikuan.gu@gmail.com>
2022-07-17 22:31:45 +0800

    misc

---
+++ BLfr2b/README.md
@@ -0,0 +1,7 @@
+# factory_bot
+
+
+
+## Ref
+
+* [https://github.com/thoughtbot/factory_bot](https://github.com/thoughtbot/factory_bot)
---
+++ BLfr2b/factory_bot.rb
@@ -0,0 +1,5 @@
+# https://www.rubydoc.info/gems/factory_bot/file/GETTING_STARTED.md#rspec
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
